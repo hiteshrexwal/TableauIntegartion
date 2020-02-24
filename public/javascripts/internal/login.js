2b02123b3a6ce5242b77af074a9d74fa8424970f
hiteshrexwal <hiteshrexwal03@gmail.com>
2020-02-24 11:58:19 +0530

    Form error show

--- public/javascripts/internal/login.js
+++ public/javascripts/internal/login.js
@@ -1,4 +1,15 @@
 const login = () => {
+
+    let username = $('input[name="username"]').val()
+    username ? '' : $('input[name="username"]').addClass('is-invalid')
+
+    let password = $('input[name="password"]').val()
+    password ? '' : $('input[name="password"]').addClass('is-invalid')
+
+    if(!username || !password){
+        return
+    } 
+
     $('.loader').show()
     $.post(`/api/login`,$('.user').serialize(),(response) => {
         
@@ -14,4 +25,12 @@ const login = () => {
         }
         $('.loader').hide()
     })
+}
+
+const onValueChange = (el) => {
+    if(!$(el).val().length){
+        $(el).addClass('is-invalid')
+        return
+    }  
+    $(el).removeClass('is-invalid')
 }
